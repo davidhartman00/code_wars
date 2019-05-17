f20e398fbafb2648f257ea8f082da0271f4e330f
davidhartman00 <davidhartman00@gmail.com>
2019-05-16 17:29:20 -0700

    added final code

--- Count_the_Smiley_Faces.js
+++ Count_the_Smiley_Faces.js
@@ -19,3 +19,11 @@ countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
 Note: In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same
 
 */
+
+// FINAL CODE
+
+
+countSmileys = (arr) => {
+    // take arr and filter out all false tests. measure final array for result
+    return arr.filter(el => RegExp("^[\:\;][\-\~]?[\)D]").test(el)).length
+}
