0acf8611f12cc7200a3cb660339e324f3d5356fa
davidhartman00 <davidhartman00@gmail.com>
2019-05-20 19:54:29 -0700

    solution was easy

--- DNA_pairing.js
+++ DNA_pairing.js
@@ -10,3 +10,16 @@ For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
 The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 */
 
+const pairElement = str => {
+    return str.split("").map(a => {
+        switch (a) {
+            case 'A': return str = ["A", 'T'];
+            case 'T': return str = ["T", 'A'];
+            case 'C': return str = ["C", 'G'];
+            case 'G': return str = ["G", 'C'];
+        }
+    }
+    )
+}
+
+pairElement("GCG");
