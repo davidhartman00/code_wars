fffdaa9f945c19bc0d49e24928603f6e412f93de
davidhartman00 <davidhartman00@gmail.com>
2019-05-21 23:35:01 -0700

    added comments

--- missing_letters.js
+++ missing_letters.js
@@ -1,3 +1,16 @@
 /* DIRECTIONS
+Find the missing letter in the passed letter range and return it.
 
+If all letters are present in the range, return undefined.
+
+Examples:
+fearNotLetter("abce") should return "d".
+  
+fearNotLetter("abcdefghjklmno") should return "i".
+  
+fearNotLetter("stvwx") should return "u".
+  
+fearNotLetter("bcdf") should return "e".
+  
+fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined.
 */
