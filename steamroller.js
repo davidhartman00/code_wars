213587754931d6d2c8e4c859abc5f28f7163cae8
davidhartman00 <davidhartman00@gmail.com>
2019-05-22 20:21:56 -0700

    directions added

---
+++ steamroller.js
@@ -0,0 +1,6 @@
+/* DIRECTIONS
+Flatten a nested array. You must account for varying levels of nesting.
+
+Example: steamrollArray([1, [2], [3, [[4]]]]) yeilds a one dimensional
+array [1,2,3,4]
+ */
