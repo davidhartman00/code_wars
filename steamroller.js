fe0ffbf371e1c52fc1ab0e1b625a1512919cb9bb
davidhartman00 <davidhartman00@gmail.com>
2019-05-22 21:27:24 -0700

    bug fix

--- steamroller.js
+++ steamroller.js
@@ -4,3 +4,15 @@ Flatten a nested array. You must account for varying levels of nesting.
 Example: steamrollArray([1, [2], [3, [[4]]]]) yeilds a one dimensional
 array [1,2,3,4]
  */
+
+const steamrollArray = arr => {
+    return arr.flatMap(el => {
+        const flatten = val => {
+            return !Array.isArray(val) ? val :
+                val.flatMap(v => flatten(v))
+        }
+        return flatten(el)
+    })
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
