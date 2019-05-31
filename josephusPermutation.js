93d829c2e0e8fa0ec149e125d3d8daa11e25d3d8
davidhartman00 <davidhartman00@gmail.com>
2019-05-31 10:43:41 -0700

    directions updated for new problem

--- josephusPermutation.js
+++ josephusPermutation.js
@@ -1,14 +1,26 @@
 /**
  DIRECTIONS:
- The maximum sum subarray problem consists in finding the maximum sum of a contiguous 
- subsequence in an array or list of integers:
+This problem takes its name by arguably the most important event in the life of the ancient historian Josephus: according to his tale, he and his 40 soldiers were trapped in a cave by the Romans during a siege.
 
-maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
-// should be 6: [4, -1, 2, 1]
-Easy case is when the list is made up of only positive numbers and the maximum sum is 
-the sum of the whole array. If the list is made up of only negative numbers, return 0
-instead.
+Refusing to surrender to the enemy, they instead opted for mass suicide, with a twist: they formed a circle and proceeded to kill one man every three, until one last man was left (and that it was supposed to kill himself to end the act).
 
-Empty list is considered to have zero greatest sum. Note that the empty list or array 
-is also a valid sublist/subarray.
- */
+Well, Josephus and another man were the last two and, as we now know every detail of the story, you may have correctly guessed that they didn't exactly follow through the original idea.
+
+You are now to create a function that returns a Josephus permutation, taking as parameters the initial array/list of items to be permuted as if they were in a circle and counted out every k places until none remained.
+
+Tips and notes: it helps to start counting from 1 up to n, instead of the usual range 0..n-1; k will always be >=1.
+
+For example, with n=7 and k=3 josephus(7,3) should act this way.
+
+[1,2,3,4,5,6,7] - initial sequence
+[1,2,4,5,6,7] => 3 is counted out and goes into the result [3]
+[1,2,4,5,7] => 6 is counted out and goes into the result [3,6]
+[1,4,5,7] => 2 is counted out and goes into the result [3,6,2]
+[1,4,5] => 7 is counted out and goes into the result [3,6,2,7]
+[1,4] => 5 is counted out and goes into the result [3,6,2,7,5]
+[4] => 1 is counted out and goes into the result [3,6,2,7,5,1]
+[] => 4 is counted out and goes into the result [3,6,2,7,5,1,4]
+So our final result is:
+
+josephus([1,2,3,4,5,6,7],3)==[3,6,2,7,5,1,4]
+For more info, browse the Josephus Permutation page on wikipedia;  */
